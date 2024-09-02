01aa1dce114a4b9f49981db3dbb7adb2ef7d4ebc
shinji0210 <jsys.iwatsubo21120053@gmail.com>
2024-09-02 13:51:21 +0900

    更新 13:51

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--タイトル -->
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World</p>'; ?>
+    </body>
+</html>
